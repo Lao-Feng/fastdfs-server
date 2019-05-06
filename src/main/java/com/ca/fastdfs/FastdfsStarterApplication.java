bbb88ac8dba10013e557ff2c7b971cc2253e21f3
fengjie <fengjie@wq99.cn>
2019-05-06 13:50:42 +0800

    修改依赖

--- src/main/java/com/ca/fastdfs/FastdfsStarterApplication.java
+++ src/main/java/com/ca/fastdfs/FastdfsStarterApplication.java
@@ -3,18 +3,13 @@ package com.ca.fastdfs;
 import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
-import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * @author 冯杰
  */
-@Import(FdfsClientConfig.class)
-// 解决jmx重复注册bean的问题
-
-@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication
+@Import(FdfsClientConfig.class)
 public class FastdfsStarterApplication {
 
     public static void main(String[] args) {
